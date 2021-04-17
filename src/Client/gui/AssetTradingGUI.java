91b8f46fa87e60ef3c96a485d3308758e07e1a38
Ben Rogers <benrogers1299@outlook.com>
2021-04-17 14:08:46 +1000

    Setup GUI

--- src/Client/gui/AssetTradingGUI.java
+++ src/Client/gui/AssetTradingGUI.java
@@ -1,9 +1,25 @@
 package Client.gui;
 
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * Point of entry for the GUI of the program
  */
 
-public class AssetTradingGUI {
+public class AssetTradingGUI extends JFrame {
 
+    public AssetTradingGUI() {
+        super("Asset Trading");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setPreferredSize(new Dimension(1000, 700));
+        pack();
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new AssetTradingGUI();
+    }
 }
+
