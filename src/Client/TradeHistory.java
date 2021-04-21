acac87c43a3aed239a0b046931c616a6d13d0de0
don <misurakalu@gmail.com>
2021-04-21 12:20:28 +1000

    Attempting to get information from trade and asset class

--- src/Client/TradeHistory.java
+++ src/Client/TradeHistory.java
@@ -1,4 +1,67 @@
 package Client;
 
+import java.time.LocalDate;
+
+/**
+ * Trade History gives the information about the asset and its trade
+ * value along with the users that have traded it for users to know if they should
+ * buy that asset.
+ */
 public class TradeHistory {
+
+    // Asset type
+    private String assetName;
+    //
+    private Double price;
+    //
+    private Integer quantity;
+    //
+    private LocalDate date;
+    //
+    private String username;
+    //
+    private String organisationNames;
+
+    public String user;
+
+    public String organization;
+
+
+    public TradeHistory()
+    {
+
+    }
+
+    /***
+     *
+     */
+    public void getTrade()
+    {
+        // When asset is being traded
+        Trades  t = new Trades(organization,user);
+        t.getListing();
+
+        // Get the information about it
+
+        //Asset a = new Assets();
+        //a.addAsset();
+    }
+
+
+
+    /***
+     * This class adds the information of the trade
+     *
+     * @param assetName
+     * @param price
+     * @param quantity
+     * @param date
+     * @param organisationNames
+     */
+    public void addTrade(String assetName, Double price, Integer quantity, LocalDate date, String organisationNames)
+    {
+
+    }
+
+
 }
--- test/Client/TradeHistoryTest.java
+++ test/Client/TradeHistoryTest.java
@@ -1,4 +1,23 @@
 package Client;
 
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
 public class TradeHistoryTest {
+
+    @BeforeAll
+    public void ConnectToDatabase()
+    {
+
+    }
+
+    @Test
+    public void TestTrade()
+    {
+
+    }
+
+
+
 }
