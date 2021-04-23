cd28145f1d063d57157f70dfe0e6a0dd0720203d
rp27 <me@example.com>
2021-04-23 18:05:01 +1000

    Modified addExtendedTradeHistory method in TradeHistory

--- src/Client/TradeHistory.java
+++ src/Client/TradeHistory.java
@@ -30,20 +30,26 @@ public class TradeHistory {
      * @return Summary data of sell listings
      */
     public void addExtendedTradeHistory() {
+        // Map of all trade listings
         Map<Integer, ArrayList<String>> multiValueMap = trades.getListing();
 
         // Iterates through multi value map to get all partially completed or completed trades
         for (Map.Entry<Integer, ArrayList<String>> mvMap : multiValueMap.entrySet()) {
-            if(multiValueMap.size() <= mvMap.getKey()) {
+            if(mvMap.getKey() <= multiValueMap.size()) {
+
                 // The key of the map is the trade ID
                 Integer tradeID = mvMap.getKey();
 
+                // String representation of trade status
                 String tradeStatus = multiValueMap.get(tradeID).get(tradeFulfilled);
+
+                // Checks whether trade has been completed or partially completed
                 boolean tradeCompleted = tradeStatus == "Yes" || tradeStatus == "Partial";
 
                 // TradeIDs for the extended trade history
                 int index = extendedTradeHistory.size() + 1;
 
+                // If a trade is completed or partially completed, adds it to the map
                 if(tradeCompleted) {
                     extendedTradeHistory.put(index, multiValueMap.get(tradeID));
 
