9d2d0e04db9a9dc17df09d11607438df293ac587
Ben Rogers <47819009+ben04rogers@users.noreply.github.com>
2021-04-25 17:16:30 +1000

    Organise GUI code into setup functions

--- src/Client/gui/AssetTradingGUI.java
+++ src/Client/gui/AssetTradingGUI.java
@@ -24,7 +24,10 @@ public class AssetTradingGUI extends JFrame implements ActionListener {
     private JPanel accountPanel;
     private JPanel assetsPanel;
     private JPanel notificationsPanel;
-    private static JTextField searchBox;
+    private JTextField searchBox;
+    private JPanel topMenuPanel;
+    private JPanel topLeft;
+    private JPanel bottomLeft;
 
     private CardLayout cardLayout = new CardLayout();
 
@@ -50,120 +53,19 @@ public class AssetTradingGUI extends JFrame implements ActionListener {
         // Current Trades object
         this.allTrades = trades;
 
+        // Setup of main frame
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setPreferredSize(new Dimension(1000, 700));
         setLayout(new BorderLayout(0, 0));
 
-        JPanel leftMenuPanel = new JPanel();
-        JPanel topMenuPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT,10,15));
-
-        leftMenuPanel.setPreferredSize(new Dimension(140, 0));
-        leftMenuPanel.setLayout(new BorderLayout());
-        leftMenuPanel.setBackground(Utility.DARKGREY);
-        add(leftMenuPanel, BorderLayout.WEST);
-        JPanel topLeft = new JPanel();
-        JPanel bottomLeft = new JPanel();
-        topLeft.setBackground(Utility.DARKGREY);
-        bottomLeft.setBackground(Utility.DARKGREY);
-
-        topMenuPanel.setPreferredSize(new Dimension(0, 60));
-        topMenuPanel.setBackground(Utility.PRIMARYBLUE);
-        add(topMenuPanel, BorderLayout.NORTH);
-
-        ImageIcon icon = new ImageIcon(this.getClass().getResource("images/bell.png"));
-
-        // Buttons in left menu
-        JButton[] leftMenuButtons = new JButton[4];
-        JButton[] topMenuButtons = new JButton[4];
-
-        for (int i = 0; i < 4; i++) {
-            String btnName = "";
-            switch(i) {
-                case 0:
-                    btnName = "Notifications";
-                    break;
-                case 1:
-                    btnName = "My Listings";
-                    icon = new ImageIcon(this.getClass().getResource("images/mylistings.png"));
-                    break;
-                case 2:
-                    btnName = "View Assets";
-                    icon = new ImageIcon(this.getClass().getResource("images/viewassets.png"));
-                    break;
-                case 3:
-                    btnName = "Logout";
-                    icon = new ImageIcon(this.getClass().getResource("images/logout.png"));
-                    break;
-
-            }
-            JButton btn = new JButton(btnName, icon);
-            btn.setBorderPainted(false);
-            btn.setBackground(Utility.DARKGREY);
-            btn.setForeground(Color.WHITE);
-            btn.setPreferredSize(new Dimension(125, 40));
-            btn.addActionListener(this);
-            leftMenuButtons[i] = btn;
-            if(btnName.equals("Logout")) {
-                bottomLeft.add(btn, BorderLayout.SOUTH);
-            } else  {
-                topLeft.add(btn);
-            }
-        }
-        leftMenuPanel.add(topLeft);
-        leftMenuPanel.add(bottomLeft, BorderLayout.SOUTH);
-
-        // Welcome message
-        JLabel welcomeMessage = new JLabel("Welcome, " + userLoggedIn.getUsername() + "!");
-        welcomeMessage.setFont(new Font("Myriad Pro",Font.BOLD,16));
-        welcomeMessage.setForeground(Color.WHITE);
-        welcomeMessage.setPreferredSize(new Dimension(240, 25));
-        topMenuPanel.add(welcomeMessage);
-
-        // Just using these to test layout
-        JLabel searchLabel = new JLabel("Search assets");
-        searchBox = new JTextField(10);
-        searchLabel.setForeground(Color.WHITE);
-        topMenuPanel.add(searchLabel);
-        topMenuPanel.add(searchBox);
-
-        // Buttons in top menu (Will come back and refactor)
-        for (int i = 0; i < 3; i++) {
-            String btnName = "";
-            switch(i) {
-                case 0:
-                    btnName = "Buy";
-                    icon = new ImageIcon(this.getClass().getResource("images/buy.png"));
-                    break;
-                case 1:
-                    btnName = "Sell";
-                    icon = new ImageIcon(this.getClass().getResource("images/sell.png"));
-                    break;
-                case 2:
-                    btnName = "Account";
-                    icon = new ImageIcon(this.getClass().getResource("images/account.png"));
-                    break;
-            }
-            JButton btn = new JButton(btnName, icon);
-            btn.setBorderPainted(false);
-            btn.setBackground(Utility.PRIMARYBLUE);
-            btn.setForeground(Color.WHITE);
-            btn.setPreferredSize(new Dimension(120, 30));
-            btn.addActionListener(this);
-
-            topMenuButtons[i] = btn;
-            topMenuPanel.add(btn);
-        }
-
-        JLabel creditsLabel =
-                new JLabel("Credits: [" + org.getOrganisation(userLoggedIn.getOrganisationName()).getCredits() + "]");
-        creditsLabel.setPreferredSize(new Dimension(100, 30));
-        creditsLabel.setForeground(Color.WHITE);
-        topMenuPanel.add(creditsLabel);
-
-        // Main content area
+        // Main content in center
         maincontent = new JPanel();
         maincontent.setLayout(cardLayout);
         add(maincontent, BorderLayout.CENTER);
+
+        // Top and left menus
+        addTopMenu();
+        addSideMenu();
 
         // Buy Panel
         buyPanel = new JPanel();
@@ -278,6 +180,111 @@ public class AssetTradingGUI extends JFrame implements ActionListener {
         Trades trades = new Trades(organisation, user);
         trades.createListing("test", "Sell", "Hardware Resources", 10, 25);
         new LoginGUI(user, user, organisation, assets, trades);
+    }
+
+    public void addTopMenu() {
+        topMenuPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT,10,15));
+        topMenuPanel.setPreferredSize(new Dimension(0, 60));
+        topMenuPanel.setBackground(Utility.PRIMARYBLUE);
+        add(topMenuPanel, BorderLayout.NORTH);
+
+        // Welcome message
+        JLabel welcomeMessage = new JLabel("Welcome, " + userLoggedIn.getUsername() + "!");
+        welcomeMessage.setFont(new Font("Myriad Pro",Font.BOLD,16));
+        welcomeMessage.setForeground(Color.WHITE);
+        welcomeMessage.setPreferredSize(new Dimension(240, 25));
+        topMenuPanel.add(welcomeMessage);
+
+        // Search box
+        JLabel searchLabel = new JLabel("Search assets");
+        searchBox = new JTextField(10);
+        searchLabel.setForeground(Color.WHITE);
+        topMenuPanel.add(searchLabel);
+        topMenuPanel.add(searchBox);
+
+        ImageIcon icon = new ImageIcon(this.getClass().getResource("images/bell.png"));
+
+        // Buttons in top menu (Will come back and refactor)
+        for (int i = 0; i < 3; i++) {
+            String btnName = "";
+            switch(i) {
+                case 0:
+                    btnName = "Buy";
+                    icon = new ImageIcon(this.getClass().getResource("images/buy.png"));
+                    break;
+                case 1:
+                    btnName = "Sell";
+                    icon = new ImageIcon(this.getClass().getResource("images/sell.png"));
+                    break;
+                case 2:
+                    btnName = "Account";
+                    icon = new ImageIcon(this.getClass().getResource("images/account.png"));
+                    break;
+            }
+            JButton btn = new JButton(btnName, icon);
+            btn.setBorderPainted(false);
+            btn.setBackground(Utility.PRIMARYBLUE);
+            btn.setForeground(Color.WHITE);
+            btn.setPreferredSize(new Dimension(120, 30));
+            btn.addActionListener(this);
+            topMenuPanel.add(btn);
+        }
+
+        JLabel creditsLabel =
+                new JLabel("Credits: [" + org.getOrganisation(userLoggedIn.getOrganisationName()).getCredits() + "]");
+        creditsLabel.setPreferredSize(new Dimension(100, 30));
+        creditsLabel.setForeground(Color.WHITE);
+        topMenuPanel.add(creditsLabel);
+    }
+
+    public void addSideMenu() {
+
+        JPanel leftMenuPanel = new JPanel();
+        leftMenuPanel.setPreferredSize(new Dimension(140, 0));
+        leftMenuPanel.setLayout(new BorderLayout());
+        leftMenuPanel.setBackground(Utility.DARKGREY);
+        add(leftMenuPanel, BorderLayout.WEST);
+        topLeft = new JPanel();
+        bottomLeft = new JPanel();
+        topLeft.setBackground(Utility.DARKGREY);
+        bottomLeft.setBackground(Utility.DARKGREY);
+        leftMenuPanel.add(topLeft);
+        leftMenuPanel.add(bottomLeft, BorderLayout.SOUTH);
+
+        ImageIcon icon = new ImageIcon(this.getClass().getResource("images/bell.png"));
+
+        for (int i = 0; i < 4; i++) {
+            String btnName = "";
+            switch(i) {
+                case 0:
+                    btnName = "Notifications";
+                    break;
+                case 1:
+                    btnName = "My Listings";
+                    icon = new ImageIcon(this.getClass().getResource("images/mylistings.png"));
+                    break;
+                case 2:
+                    btnName = "View Assets";
+                    icon = new ImageIcon(this.getClass().getResource("images/viewassets.png"));
+                    break;
+                case 3:
+                    btnName = "Logout";
+                    icon = new ImageIcon(this.getClass().getResource("images/logout.png"));
+                    break;
+
+            }
+            JButton btn = new JButton(btnName, icon);
+            btn.setBorderPainted(false);
+            btn.setBackground(Utility.DARKGREY);
+            btn.setForeground(Color.WHITE);
+            btn.setPreferredSize(new Dimension(125, 40));
+            btn.addActionListener(this);
+            if(btnName.equals("Logout")) {
+                bottomLeft.add(btn, BorderLayout.SOUTH);
+            } else  {
+                topLeft.add(btn);
+            }
+        }
     }
 
     @Override
